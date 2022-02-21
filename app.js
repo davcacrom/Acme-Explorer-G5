6a6e03536140ade35bb35b499fff0f7493ee51c7
reyblacua <reyblacua@alum.us.es>
2022-02-21 18:46:46 +0100

    Incorporación de conexión con usuario
    Código actualizado para que se pueda realizar la conexión con los credenciales de un usuario. Añadido fichero de datos de actores. Añadida colección de postman

--- app.js
+++ app.js
@@ -24,12 +24,30 @@ routesFinder(app)
 routesTrip(app)
 
 // MongoDB URI building
+const mongoDBUser = process.env.mongoDBUser || 'myUser'
+const mongoDBPass = process.env.mongoDBPass || 'myUserPassword'
+const mongoDBCredentials = (mongoDBUser && mongoDBPass) ? mongoDBUser + ':' + mongoDBPass + '@' : ''
+
 const mongoDBHostname = process.env.mongoDBHostname || 'localhost'
 const mongoDBPort = process.env.mongoDBPort || '27017'
 const mongoDBName = process.env.mongoDBName || 'ACME-Explorer'
+
+//TODO: Descomentar si no se tiene usuario en bd
 const mongoDBURI = 'mongodb://' + mongoDBHostname + ':' + mongoDBPort + '/' + mongoDBName
 
+// const mongoDBURI = 'mongodb://' + mongoDBCredentials + mongoDBHostname + ':' + mongoDBPort + '/' + mongoDBName
+
+// TODO: Descomentar si no se tiene usuario creado en la bd
 mongoose.connect(mongoDBURI)
+
+//TODO: Si no se tiene usuario en la bd, diapositivas 9 y 10 de NoSQL Features and JSON Storage
+// mongoose.connect(mongoDBURI, {
+//   connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+//   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+//   family: 4, // skip trying IPv6
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
 console.log('Connecting DB to: ' + mongoDBURI)
 
 mongoose.connection.on('open', function () {
