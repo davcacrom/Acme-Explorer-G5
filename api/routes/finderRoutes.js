31078885ae27a1576c25d2864435803a73116529
reyblacua <reyblacua@alum.us.es>
2022-03-11 20:06:19 +0100

    Cambio en la carga de datos de Trips y generación de endpoints de v2

--- api/routes/finderRoutes.js
+++ api/routes/finderRoutes.js
@@ -2,13 +2,25 @@
 module.exports = function (app) {
   const finders = require('../controllers/finderController');
   
-  app.route('/finders/dashboard')
+  // V1 - Sin autenticación
+  app.route('/v1/finders/dashboard')
     .get(finders.get_dashboard) //finder dashboard
 
-  app.route('/finders/:finderId')
+  app.route('/v1/finders/:finderId')
     .get(finders.read_a_finder) 
     .put(finders.update_a_finder) //manage finder (update)
   
-  app.route('/finders/config/:configId')
+  app.route('/v1/finders/config/:configId')
+  .put(finders.update_config) // update cached period, update limit results
+
+  // V2 - Con autenticación
+  app.route('/v2/finders/dashboard')
+    .get(finders.get_dashboard) //finder dashboard
+
+  app.route('/v2/finders/:finderId')
+    .get(finders.read_a_finder) 
+    .put(finders.update_a_finder) //manage finder (update)
+  
+  app.route('/v2/finders/config/:configId')
   .put(finders.update_config) // update cached period, update limit results
 }
--- api/routes/tripRoutes.js
+++ api/routes/tripRoutes.js
@@ -3,30 +3,57 @@ module.exports = function (app) {
   const trips = require('../controllers/tripController');
   const applications = require('../controllers/applicationController');
   
-  app.route('/trips')
+  // V1 - Sin autenticación
+  app.route('/v1/trips')
     .get(trips.list_all_trips) //browse the list of trips, search for trips using keyword, listing trips
     .post(trips.create_a_trip) //creating trips
 
-  app.route('/trips/dashboard')
+  app.route('/v1/trips/dashboard')
   .get(trips.get_dashboard) //trip dashboard
   
-  app.route('/trips/:tripId')
+  app.route('/v1/trips/:tripId')
     .get(trips.read_a_trip) //displaying trips
     .put(trips.update_a_trip) //publishing trips, modifying trips, cancel trip
     .delete(trips.delete_a_trip) //deleting trips    
 
-  app.route('/trips/:tripId/applications')
+  app.route('/v1/trips/:tripId/applications')
     .get(applications.list_all_applications) //listing applications for a trip
     .post(applications.create_an_application) //apply for a trip
 
-  app.route('/trips/finders/:finderId')
+  app.route('/v1/trips/finders/:finderId')
     .get(trips.list_trips_by_finder) //listing trips by explorers finder
 
-  app.route('/trips/:tripId/applications/:applicationId')
+  app.route('/v1/trips/:tripId/applications/:applicationId')
     .get(applications.read_an_application) //displaying applications
     .put(applications.update_an_application) //change status, cancel application
 
-  app.route('/trips/:tripId/applications/:applicationId/:price')
+  app.route('/v1/trips/:tripId/applications/:applicationId/:price')
     .put(applications.update_an_application) //pay trip
 
+  //v2 - con autorización
+  app.route('/v2/trips')
+  .get(trips.list_all_trips) //browse the list of trips, search for trips using keyword, listing trips
+  .post(trips.create_a_trip) //creating trips
+
+  app.route('/v2/trips/dashboard')
+  .get(trips.get_dashboard) //trip dashboard
+  
+  app.route('/v2/trips/:tripId')
+    .get(trips.read_a_trip) //displaying trips
+    .put(trips.update_a_trip) //publishing trips, modifying trips, cancel trip
+    .delete(trips.delete_a_trip) //deleting trips    
+
+  app.route('/v2/trips/:tripId/applications')
+    .get(applications.list_all_applications) //listing applications for a trip
+    // .post(applications.create_an_application_with_auth) //apply for a trip
+
+  app.route('/v2/trips/finders/:finderId')
+    .get(trips.list_trips_by_finder) //listing trips by explorers finder
+
+  app.route('/v2/trips/:tripId/applications/:applicationId')
+    .get(applications.read_an_application) //displaying applications
+    .put(applications.update_an_application) //change status, cancel application
+
+  app.route('/v2/trips/:tripId/applications/:applicationId/:price')
+    .put(applications.update_an_application) //pay trip
 }
