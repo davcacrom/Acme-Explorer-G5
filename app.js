151f6ee6803c8e141121d9448bdde64780c91792
reyblacua <reyblacua@alum.us.es>
2022-03-14 19:25:45 +0100

    Correcciones menores

--- app.js
+++ app.js
@@ -24,6 +24,15 @@ admin.initializeApp({
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 const routesActors = require('./api/routes/actorRoutes')
 const routesFinder = require('./api/routes/finderRoutes')
 const routesTrip = require('./api/routes/tripRoutes');
